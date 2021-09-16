5de7e50e9e617c9ace1430201787280320ce38a8
jonathan <you@example.com>
2021-09-15 21:30:50 -0500

    Started Alien Game

---
+++ AlienInvasionGame.py
@@ -0,0 +1,36 @@
+## install pip module with: python3 -m pip install --user pygame
+import sys
+
+import pygame
+
+class AlienInvasion:
+    '''Overall class to manage game assets and behavior'''
+
+    def __init__(self):
+        '''Initialize the game, and create game resources'''
+        pygame.init()
+        #This appeared on page 231 (line below)
+        pygame.display.set_caption("Alien Invasion")
+
+        #Set the background color
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        '''Start the main loop for the game'''
+        while True:
+            # watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Redraw the screen during each pass through the loop
+            self.screen.fill (self.bg_color)
+
+
+            # Make the most recently drawn screen available
+            pygame.display.flip
+
+if __name__ == '__main__':
+    #make a game instance, and run the game
+    ai =AlienInvasion()
+    ai.run_game()
