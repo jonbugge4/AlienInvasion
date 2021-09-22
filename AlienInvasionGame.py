ffa3ec691be6e4015992f6917b652607e7701f5f
jonathan <you@example.com>
2021-09-22 09:42:11 -0500

    alein updates

--- AlienInvasionGame.py
+++ AlienInvasionGame.py
@@ -3,12 +3,20 @@ import sys
 
 import pygame
 
+from settings import Settings
+
+
 class AlienInvasion:
     '''Overall class to manage game assets and behavior'''
 
     def __init__(self):
         '''Initialize the game, and create game resources'''
         pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            self.settings.screen_width, self.settings.screen_height)
+            
         #This appeared on page 231 (line below)
         pygame.display.set_caption("Alien Invasion")
 
