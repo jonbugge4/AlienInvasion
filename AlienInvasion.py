47d6a45c4fe81d74a91939841cd9fd5b362e431a
jonathan <you@example.com>
2021-10-13 15:44:28 -0500

    more work done

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -6,6 +6,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     '''Overall class to manage game assets and behavior'''
@@ -14,8 +15,6 @@ class AlienInvasion:
         '''Initialize the game, and create game resources'''
         pygame.init()
         self.settings = Settings()
-
-
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
@@ -29,6 +28,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self.__create_fleet()
 
         #Set the background color
         self.bg_color = (230, 230, 230)
@@ -95,9 +97,26 @@ class AlienInvasion:
             self.ship.blitme()
             for bullet in self.bullets.sprites():
                 bullet.draw_bullet()
+            self.aliens.draw(self.screen)
+
+    def __create_fleet(self):
+        '''Create the fleet of aliens'''
+        #Make an alien and find the number of aliens in a row
+        #Spacing between each alien is equal to one alien width
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2* alien_width)
+
+        #Create the first row of aliens
+        for alien_number in range(number_aliens_x):
+            #Create an alien and place it in the row
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.xself.aliens.add(alien)
 
             # Make the most recently drawn screen available
-            pygame.display.flip()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #make a game instance, and run the game
